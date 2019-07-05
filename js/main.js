7394523e960a9d138983a622a523072cef008c54
roddom67 <roddom67@gmail.com>
2019-07-05 20:09:28 -0300

    ajustes

--- js/main.js
+++ js/main.js
@@ -113,8 +113,14 @@ $(document).ready(function() {
   if ($("#lightgallery").length > 0) {
   		lightGallery(document.getElementById('lightgallery'), {
     		mode: 'lg-fade',
-    		cssEasing : 'cubic-bezier(0.25, 0, 0.25, 1)',
-    		share : false
+			cssEasing: 'cubic-bezier(0.25, 0, 0.25, 1)',
+			share: false,
+			controls: true,
+			counter: false,
+			zoom: false,
+			fullScreen: false,
+			autoplayControls: false,
+			download: false
     		
 		});
   }
